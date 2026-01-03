b07f76e88a09fda2ed5f8c800be97e65d74b3b79
Peter Meyer <peter.o.meyer@gmail.com>
2026-01-02 23:35:14 -0500

    feat: remove graphql multipart upload support and take dependency on new graphql multipart library

--- src/main/java/com/yellowmoon/gmdb/graphql/multipartmapper/ObjectGraphTraverser.java
+++
@@ -1,6 +0,0 @@
-package com.yellowmoon.gmdb.graphql.multipartmapper;
-
-public interface ObjectGraphTraverser {
-    <T> T set(String path, T value);
-    ObjectGraphTraverser dereference(String path);
-}
