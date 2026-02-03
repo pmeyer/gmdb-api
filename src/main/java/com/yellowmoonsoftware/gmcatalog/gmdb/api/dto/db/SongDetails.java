4de8b0915a94caec47996632ca9a1e461b5cf07d
Peter Meyer <peter.o.meyer@gmail.com>
2026-02-03 11:04:07 -0500

    chore: modify `SongDetails` to be a resource bundle

--- src/main/java/com/yellowmoonsoftware/gmcatalog/gmdb/api/dto/db/SongDetails.java
+++ src/main/java/com/yellowmoonsoftware/gmcatalog/gmdb/api/dto/db/SongDetails.java
@@ -1,8 +1,18 @@
 package com.yellowmoonsoftware.gmcatalog.gmdb.api.dto.db;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.yellowmoonsoftware.gmcatalog.gmdb.api.mybatis.type.JsonTypeHandler;
+import com.yellowmoonsoftware.gmcatalog.gmdb.api.dto.output.AbstractResourceBundle;
+import lombok.Getter;
+import lombok.experimental.Accessors;
 
-@JsonTypeHandler
+@Getter
+@Accessors(fluent = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record SongDetails(Integer trackNumber) { }
+public final class SongDetails extends AbstractResourceBundle {
+    private final Integer trackNumber;
+
+    public SongDetails(Integer trackNumber) {
+        super(null);
+        this.trackNumber = trackNumber;
+    }
+}
