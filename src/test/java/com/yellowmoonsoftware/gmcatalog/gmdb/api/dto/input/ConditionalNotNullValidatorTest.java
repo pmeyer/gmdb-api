2a463f5531b605500b02d44e890c6d83d32b6bc6
Peter Meyer <peter.o.meyer@gmail.com>
2026-05-04 18:16:39 -0400

    chore: wrote some simple integration tests for the custom validator

--- src/test/java/com/yellowmoonsoftware/gmcatalog/gmdb/api/dto/input/ConditionalNotNullValidatorTest.java
+++ src/test/java/com/yellowmoonsoftware/gmcatalog/gmdb/api/dto/input/ConditionalNotNullValidatorTest.java
@@ -1,5 +1,6 @@
 package com.yellowmoonsoftware.gmcatalog.gmdb.api.dto.input;
 
+import com.yellowmoonsoftware.gmcatalog.gmdb.api.dto.ArtistType;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.ValidatorFactory;
@@ -7,20 +8,42 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ConditionalNotNullValidatorTest {
 
     @Test
-    void testValidation() {
-
-        final GqlInputTypes.ArtistInput foo = new GqlInputTypes.ArtistInput(null, "Foo", null);
+    void testValid() {
+        final ArtistInput foo = new ArtistInput(null, new ArtistData("Foo Fighters", ArtistType.BAND));
 
         try (final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory()) {
 
-            Set<ConstraintViolation<GqlInputTypes.ArtistInput>> validate = validatorFactory.getValidator().validate(foo);
-            System.out.println(validate);
+            final Set<ConstraintViolation<ArtistInput>> validate = validatorFactory.getValidator().validate(foo);
+            assertThat(validate).isEmpty();
         }
     }
 
+    @Test
+    void testInvalid() {
+        final ArtistInput foo = new ArtistInput(null, null);
+
+        try (final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory()) {
+
+            final Set<ConstraintViolation<ArtistInput>> validate = validatorFactory.getValidator().validate(foo);
+            assertThat(validate).extracting(ConstraintViolation::getMessage).containsExactly("ArtistInput must have an ID or data");
+        }
+    }
+
+    @Test
+    void testInvalidCascade() {
+        final ArtistInput foo = new ArtistInput(null, new ArtistData(null, null));
+
+        try (final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory()) {
+
+            final Set<ConstraintViolation<ArtistInput>> validate = validatorFactory.getValidator().validate(foo);
+
+            assertThat(validate).extracting(ConstraintViolation::getMessage)
+                    .allMatch(m -> m.contains("must not be null"));
+        }
+    }
 }
