fe6a62573bb3736768af5db8bcb3bfdd7a1fa6da
Peter Meyer <peter.o.meyer@gmail.com>
2026-06-13 18:38:17 -0400

    feat: filter publications by index
    Add pubIndexId to pubSearch criteria and filter results by publication index.

--- src/test/java/com/yellowmoonsoftware/gmcatalog/gmdb/api/dto/input/PubSearchCriteriaTest.java
+++ src/test/java/com/yellowmoonsoftware/gmcatalog/gmdb/api/dto/input/PubSearchCriteriaTest.java
@@ -14,9 +14,10 @@ class PubSearchCriteriaTest {
         final LocalDate dateStart = LocalDate.of(2020, 1, 1);
         final LocalDate dateEnd = LocalDate.of(2024, 1, 1);
 
-        final PubSearchCriteria criteria = new PubSearchCriteria(1L, "guide", PubType.BOOK, dateStart, dateEnd, true);
+        final PubSearchCriteria criteria = new PubSearchCriteria(1L, 2L, "guide", PubType.BOOK, dateStart, dateEnd, true);
 
         assertThat(criteria.id()).isEqualTo(1L);
+        assertThat(criteria.pubIndexId()).isEqualTo(2L);
         assertThat(criteria.searchName()).isEqualTo("guide");
         assertThat(criteria.type()).isEqualTo(PubType.BOOK);
         assertThat(criteria.dateStart()).isEqualTo(dateStart);
@@ -26,14 +27,23 @@ class PubSearchCriteriaTest {
 
     @Test
     void supportsRecordEqualityAndStringRepresentation() {
-        final PubSearchCriteria criteria = new PubSearchCriteria(1L, "guide", PubType.BOOK, LocalDate.of(2020, 1, 1), LocalDate.of(2024, 1, 1), true);
-        final PubSearchCriteria sameValues = new PubSearchCriteria(1L, "guide", PubType.BOOK, LocalDate.of(2020, 1, 1), LocalDate.of(2024, 1, 1), true);
-        final PubSearchCriteria differentType = new PubSearchCriteria(1L, "guide", PubType.MAG, LocalDate.of(2020, 1, 1), LocalDate.of(2024, 1, 1), true);
+        final PubSearchCriteria criteria = new PubSearchCriteria(1L, 2L, "guide", PubType.BOOK, LocalDate.of(2020, 1, 1), LocalDate.of(2024, 1, 1), true);
+        final PubSearchCriteria sameValues = new PubSearchCriteria(1L, 2L, "guide", PubType.BOOK, LocalDate.of(2020, 1, 1), LocalDate.of(2024, 1, 1), true);
+        final PubSearchCriteria differentPubIndex = new PubSearchCriteria(1L, 3L, "guide", PubType.BOOK, LocalDate.of(2020, 1, 1), LocalDate.of(2024, 1, 1), true);
 
         assertThat(criteria)
             .isEqualTo(sameValues)
             .hasSameHashCodeAs(sameValues)
-            .isNotEqualTo(differentType);
-        assertThat(criteria.toString()).contains("id=1", "searchName=guide", "hasTranscriptions=true");
+            .isNotEqualTo(differentPubIndex);
+        assertThat(criteria.toString()).contains("id=1", "pubIndexId=2", "searchName=guide", "hasTranscriptions=true");
+    }
+
+    @Test
+    void supportsPublicationIndexOnlyCriteria() {
+        final PubSearchCriteria criteria = new PubSearchCriteria(null, 2L, null, null, null, null, null);
+
+        assertThat(criteria.id()).isNull();
+        assertThat(criteria.pubIndexId()).isEqualTo(2L);
+        assertThat(criteria.searchName()).isNull();
     }
 }
